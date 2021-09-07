1ad6c7ef77aa8ed410c3cc5602cfaf220c2c218d
Pablo Von Knoblauch Fontão <pablo-fontao@hotmail.com>
2021-09-07 18:44:58 -0300

    Conclusão do capítulo 3!

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,4 +1,4 @@
-from impostos import ISS, ICMS
+from impostos import ISS, ICMS, ICPP, IKCV
 
 
 class CalculadoraDeImpostos(object):
@@ -9,10 +9,18 @@ class CalculadoraDeImpostos(object):
 
 
 if __name__ == '__main__':
-    from orcamento import Orcamento
+    from orcamento import Orcamento, Item
 
     calculador = CalculadoraDeImpostos()
-    orcamento_teste = Orcamento(500)
+    orcamento_teste = Orcamento()
+    orcamento_teste.adiciona_item(Item('ITEM - 1', 50))
+    orcamento_teste.adiciona_item(Item('ITEM - 2', 200))
+    orcamento_teste.adiciona_item(Item('ITEM - 3', 250))
 
+    print('ISS e ICMS')
     calculador.realiza_calculo(orcamento_teste, ISS)
     calculador.realiza_calculo(orcamento_teste, ICMS)
+
+    print('ICPP e IKCV')
+    calculador.realiza_calculo(orcamento_teste, ICPP())
+    calculador.realiza_calculo(orcamento_teste, IKCV())
--- impostos.py
+++ impostos.py
@@ -1,3 +1,26 @@
+from abc import ABCMeta, abstractmethod
+
+
+class TemplateImpostoCondicional(metaclass=ABCMeta):
+    def calcula(self, orcamento):
+        if self.deve_usar_maxima_taxacao(orcamento):
+            return self.maxima_taxacao(orcamento)
+        else:
+            return self.minima_taxacao(orcamento)
+
+    @abstractmethod
+    def deve_usar_maxima_taxacao(self, orcamento):
+        pass
+
+    @abstractmethod
+    def maxima_taxacao(self, orcamento):
+        pass
+
+    @abstractmethod
+    def minima_taxacao(self, orcamento):
+        pass
+
+
 class ISS:
     @staticmethod
     def calcula(orcamento) -> float:
@@ -8,3 +31,32 @@ class ICMS:
     @staticmethod
     def calcula(orcamento) -> float:
         return orcamento.valor * 0.06
+
+
+class ICPP(TemplateImpostoCondicional):
+    def deve_usar_maxima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor > 500
+
+    def maxima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor * 0.07
+
+    def minima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor * 0.05
+
+
+class IKCV(TemplateImpostoCondicional):
+    def deve_usar_maxima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor > 500 and self.__tem_item_maior_que_100_reais(orcamento)
+
+    def maxima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor * 0.1
+
+    def minima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor * 0.06
+
+    @staticmethod
+    def __tem_item_maior_que_100_reais(orcamento) -> bool:
+        for item in orcamento.obter_itens():
+            if item.valor > 100:
+                return True
+        return False
