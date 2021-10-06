ff89249e5fcf857debf0cf3aa2b6306a9e0a4d16
Pablo Von Knoblauch Fontão <pablo-fontao@hotmail.com>
2021-10-06 14:01:34 -0300

    Conclusão do curso de Patterns Python I

--- nota_fiscal.py
+++ nota_fiscal.py
@@ -22,7 +22,8 @@ class NotaFiscal:
                  cnpj: str,
                  itens: List[Item],
                  data_de_emissao: date = date.today(),
-                 detalhes: str = ''):
+                 detalhes: str = '',
+                 observadores: List[any] = []):
         self._razao_social: str = razao_social
         self._cnpj: str = cnpj
         self._data_de_emissao: date = data_de_emissao
@@ -30,6 +31,9 @@ class NotaFiscal:
             raise Exception('Detalhes da nota não podem ter mais que 20 caracteres')
         self._detalhes: str = detalhes
         self._itens: List[Item] = itens
+
+        for observador in observadores:
+            observador(self)
 
     @property
     def razao_social(self) -> str:
@@ -51,6 +55,8 @@ class NotaFiscal:
 if __name__ == "__main__":
     from criador_de_nota_fiscal import CriadorNotaFiscal
 
+    from observadores import *
+
 
     itens_teste: List[Item] = [
         Item(
@@ -66,13 +72,14 @@ if __name__ == "__main__":
     nota_fiscal: NotaFiscal = NotaFiscal(
         razao_social="FHSA Limitada",
         cnpj="012345678901234",
-        itens=itens_teste
+        itens=itens_teste,
+        observadores=[imprime, envia_por_email, salva_no_banco]
     )
     print(nota_fiscal.__dict__)
 
-    nota_fiscal_criada_com_builder = (CriadorNotaFiscal()
-                                      .com_razao_social("FHSA Limitada")
-                                      .com_cnpj("012345678901234")
-                                      .com_itens(itens_teste)
-                                      .controi())
-    print(nota_fiscal_criada_com_builder.__dict__)
+    # nota_fiscal_criada_com_builder = (CriadorNotaFiscal()
+    #                                   .com_razao_social("FHSA Limitada")
+    #                                   .com_cnpj("012345678901234")
+    #                                   .com_itens(itens_teste)
+    #                                   .controi())
+    # print(nota_fiscal_criada_com_builder.__dict__)
